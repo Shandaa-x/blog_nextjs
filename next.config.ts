42d31a414b9bea2fdae2703dc5a2018262dc7757
Xiand <thekingofpirate2003@gmail.com>
2025-04-30 20:11:49 +0800

    5rd commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'page.tsx', 'page.ts'],
   reactStrictMode: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
