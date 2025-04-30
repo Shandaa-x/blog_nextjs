2033474ebca6ae51c2778f56b7b63331faed3a9c
Xiand <thekingofpirate2003@gmail.com>
2025-05-01 02:03:58 +0800

    something

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,13 @@
-// "use client";
+// app/register/page.tsx
+import dynamic from "next/dynamic";
+
 export const metadata = {
   title: "Бүртгүүлэх",
 };
 
-import RegisterPageView from "./RegisterPage";
+const RegisterPageView = dynamic(() => import("./RegisterPage"), {
+  ssr: false, // ✅ disables server-side rendering for this component
+});
 
 export default function Register() {
   return <RegisterPageView />;
